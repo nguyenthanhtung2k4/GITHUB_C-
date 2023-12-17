9f87e0122ad729af3c059d60c477158d11a8adb9
JUSST <hoangsontung2k4@gmail.com>
2023-12-17 22:40:45 +0700

    logic

--- tai_nang_lap_trinh/lam_viec_voi_mang.cpp
+++ tai_nang_lap_trinh/lam_viec_voi_mang.cpp
@@ -1,27 +1,50 @@
 #include <iostream> 
 using namespace std;
-int main(){
-     int mang[3][5]={10,10,10,10,5,5,8,8,8,3,45,45,40,45,10};
-     int flas=0;
+void sap_xep(int mang[][5] ){
      for(int i=0;i<3;i++){
+          for(int j=0;j<5;j++){
+               for(int a=0;a<4;a++){ 
+                    for(int b=0;b<4-1;b++){
+                         if(mang[i][b]>mang[i][a]){
+                              int sap;
+                              sap=mang[i][b];
+                              mang[i][b]=mang[i][b+1];
+                              mang[i][b+1]=sap;
+                         }    
+                    }
+               }    
+          }
+     }
+}
+int main(){
+     int mang[3][5]={5,9,5,5,1,12,12,12,6,6,5,5,5,6,1,};
+
+     sap_xep(mang); 
+     for(int i=0;i<3;i++){
+          for(int j=0;j<5;j++){
+               cout<<mang[i][j]<<" ";
+          }cout<<endl;
+     }
+     for(int i=0;i<3;i++){
+          int flas=0;
+          int flas2=0;          
           for(int j=0;j<5;j++){
                if(mang[i][0]==mang[i][1]&& mang[i][1]==mang[i][2]&&mang[i][2]==mang[i][3]){
                     flas=1;
                }else {
-                    for(int a=0;a<5-1;a++){ 
-                         for(int b=0;b<5-1;b++){
-                              if(mang[i][b]>mang[i][a]){
-                                   int sap;
-                                   sap=mang[i][b];
-                                   mang[i][a]=sap;
-                              }
-                              
-                         }
-                    }
-               }
+                    int sun=mang[i][0]+mang[i][4];
+                    mang[i][0]=sun;
+                    if(mang[i][0]==mang[i][1]&& mang[i][1]==mang[i][2]&&mang[i][2]==mang[i][3]){
+                         flas2=1;
+                    }else{flas2=0;}
+               }             
           }
-          if(flas==1){
+          if(flas2==1|| flas==1){
                cout<<"yes"<<endl;
           }
+          if(flas2==0||flas==0){
+               cout<<"no"<<endl;
+          }           
      }
+     return 0;
 }
---
+++ tai_nang_lap_trinh/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+     if(flas2==1|| flas==1){
+          cout<<"yes"<<endl;
+     }else{
+          cout<<"no"<<endl;
+     } 
